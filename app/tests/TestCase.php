9e3b7e7509967202bce85145341474d87785ebea
Craig Giles <craig@gilesc.com>
2014-01-12 11:02:58 -0800

    Added a base call for TestCase

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -6,12 +6,25 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
      *
      * @return \Symfony\Component\HttpKernel\HttpKernelInterface
      */
-    public function createApplication()
-    {
+    public function createApplication() {
         $unitTesting = true;
 
         $testEnvironment = 'testing';
 
-        return require __DIR__.'/../../bootstrap/start.php';
+        return require __DIR__ . '/../../bootstrap/start.php';
+    }
+
+    /**
+     * @param $method
+     * @param $args
+     * @return \Illuminate\Http\Response
+     * @throws BadMethodCallException
+     */
+    public function __call($method, $args) {
+        if (in_array($method, ['get', 'post', 'put', 'patch', 'delete'])) {
+            return $this->call($method, $args[0]);
+        }
+
+        throw new BadMethodCallException;
     }
 }
