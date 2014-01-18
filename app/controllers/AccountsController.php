c281284cef5606a190f3d25daa6554d3263942b0
Craig Giles <craig@gilesc.com>
2014-01-18 12:09:15 -0800

    changing back to array for json

--- app/controllers/AccountsController.php
+++ app/controllers/AccountsController.php
@@ -64,7 +64,7 @@ class AccountsController extends \BaseController {
 
         $return = array();
         foreach ($results as $result) {
-            $return[] = $result->toJson();
+            $return[] = $result->toArray();
         }
         $response = array('response' => 'success', 'data' => $return);
         return $response;
