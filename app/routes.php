6930a888835a6d338e8277ff21a4ffa8cf5b2061
Craig Giles <craig@gilesc.com>
2014-01-19 12:39:49 -0800

    Added basic authentication

--- app/routes.php
+++ app/routes.php
@@ -2,11 +2,14 @@
 
 //Route::any('/', array( 'as' => 'home', 'uses' => 'AccountsController@index' ));
 Route::get('/', function() {
-    echo "THIS IS MASTER";
     dd(App::environment());
 });
 
 Route::resource('accounts', 'AccountsController');
 Route::post('/accounts/create', 'AccountsController@upload');
+
+Route::get('logon', 'SessionsController@create');
+Route::get('logout', 'SessionsController@destroy');
+Route::resource('sessions', 'SessionsController');
 
 
