8f449552cc2244a961c85cb88e98bf53c2cab962
Craig Giles <craig@gilesc.com>
2014-01-20 13:49:42 -0800

    updated test

---
+++ app/tests/api/controllers/ApiAccountsControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Config;
+
+class ApiAccountsControllerTest extends TestCase {
+
+    public function test_testaccount() {
+        $json = Config::get('testdata.testaccount01');
+
+        $this->call('POST', 'api/accounts/update', array($json));
+    }
+}
+ 
