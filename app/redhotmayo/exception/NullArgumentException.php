2ff273890ff4e07e886fcfef16fb3fd9e3a7c4fc
Craig Giles <craig@gilesc.com>
2014-03-10 02:21:26 +0000

    WS-6 added a new exception class

---
+++ app/redhotmayo/exception/NullArgumentException.php
@@ -0,0 +1,13 @@
+<?php namespace redhotmayo\exception;
+
+
+use Illuminate\Support\MessageBag;
+
+class NullArgumentException extends Exception {
+    const ERROR_MESSAGE = 'Null argument detected';
+
+    public function __construct() {
+        $msg = new MessageBag(self::ERROR_MESSAGE);
+        parent::__construct($msg);
+    }
+}
