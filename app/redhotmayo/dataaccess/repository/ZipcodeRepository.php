4391ffd0256eedab4bf181955742294073668ac2
Craig Giles <craig@gilesc.com>
2014-04-05 19:58:44 -0700

    WS-17 updated repositories for zipcode and subscription

--- app/redhotmayo/dataaccess/repository/ZipcodeRepository.php
+++ app/redhotmayo/dataaccess/repository/ZipcodeRepository.php
@@ -1,6 +1,6 @@
 <?php namespace redhotmayo\dataaccess\repository;
 
-interface ZipcodeRepository {
+interface ZipcodeRepository extends Repository {
     /**
      * Obtain a list of zipcodes for the given city
      *
@@ -24,4 +24,17 @@ interface ZipcodeRepository {
      * @return array
      */
     public function getAllCounties($conditions);
+
+    /**
+     * Obtain the City, State, County, Zipcode and Population information for the
+     * given constraints.
+     *
+     * NOTE: this function is separate from find() due to the fact that it needs to be
+     * a distinct city. With the zipcode database, zipcodes are not unique, however all
+     * zipcodes point to a distinct city.
+     *
+     * @param $array
+     * @return mixed
+     */
+    public function getLocationInformation($array);
 }
