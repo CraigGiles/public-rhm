ee5a3e88327005483ab78f6dc3a331c7c55ede26
Craig Giles <craig@gilesc.com>
2014-04-05 19:53:13 -0700

    WS-17 updated DataAccessServiceProvider
    explicitly calling the Subscription Repo

--- app/redhotmayo/dataaccess/DataAccessServiceProvider.php
+++ app/redhotmayo/dataaccess/DataAccessServiceProvider.php
@@ -4,6 +4,7 @@ use Illuminate\Support\ServiceProvider;
 use redhotmayo\dataaccess\repository\sql\CuisineRepositorySQL;
 use redhotmayo\dataaccess\repository\sql\FoodServicesRepositorySQL;
 use redhotmayo\dataaccess\repository\sql\MobileDeviceRepositorySQL;
+use redhotmayo\dataaccess\repository\sql\SubscriptionRepositorySQL;
 use redhotmayo\dataaccess\repository\sql\ZipcodeRepositorySQL;
 
 class DataAccessServiceProvider extends ServiceProvider {
@@ -20,11 +21,12 @@ class DataAccessServiceProvider extends ServiceProvider {
         $this->app->bind('redhotmayo\dataaccess\repository\UserRepository', 'redhotmayo\dataaccess\repository\sql\UserRepositorySQL');
         $this->app->bind('redhotmayo\dataaccess\repository\AddressRepository', 'AddressRepositorySQL');
         $this->app->bind('redhotmayo\dataaccess\repository\NoteRepository', 'NoteRepositorySQL');
-        $this->app->bind('redhotmayo\dataaccess\repository\SubscriptionRepository', 'SubscriptionRepositorySQL');
+        $this->app->bind('redhotmayo\dataaccess\repository\SubscriptionRepository', SubscriptionRepositorySQL::SERVICE);
         $this->app->bind('redhotmayo\dataaccess\repository\ZipcodeRepository', ZipcodeRepositorySQL::SERVICE);
         $this->app->bind('redhotmayo\dataaccess\repository\CuisineRepository', CuisineRepositorySQL::SERVICE);
         $this->app->bind('redhotmayo\dataaccess\repository\FoodServicesRepository', FoodServicesRepositorySQL::SERVICE);
         $this->app->bind('redhotmayo\dataaccess\repository\MobileDeviceRepository', MobileDeviceRepositorySQL::SERVICE);
 
+        $this->app->bind('SubscriptionRepository', SubscriptionRepositorySQL::SERVICE);
     }
 }
