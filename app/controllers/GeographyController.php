d1174c184af11da6e19d71bb50107f19412453d4
Craig Giles <craig@gilesc.com>
2014-04-06 20:50:27 -0700

    WS-17 added an index to all outgoing json files for states and counties

--- app/controllers/GeographyController.php
+++ app/controllers/GeographyController.php
@@ -16,24 +16,29 @@ class GeographyController extends \BaseController {
         $cities =  $this->zipcodeRepo->getAllCities(Input::all());
         $counties =  $this->zipcodeRepo->getAllCounties(Input::all());
 
+        $fe_id = 0;
         foreach ($cities as $city) {
             $value = [
+                'id' => $fe_id,
                 'city' => $city,
                 'search_by' => $city,
                 'type' => 'city',
             ];
 
             $data[] = $value;
+            $fe_id++;
         }
 
         foreach ($counties as $county) {
             $value = [
+                'id' => $fe_id,
                 'county' => $county,
                 'search_by' => $county,
                 'type' => 'county',
             ];
 
             $data[] = $value;
+            $fe_id++;
         }
 
         return $data;
