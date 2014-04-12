69bff6fcdd49a953f5ae4fd13b295432a35d7323
Craig Giles <craig@gilesc.com>
2014-04-11 20:41:54 -0700

    WS-15 removed getEncryptedColumns() from the interface

--- app/redhotmayo/dataaccess/encryption/Encrypted.php
+++ app/redhotmayo/dataaccess/encryption/Encrypted.php
@@ -2,11 +2,6 @@
 
 interface Encrypted {
     /**
-     * Obtain a list of encrypted columns
-     */
-    public function getEncryptedColumns();
-
-    /**
      * Take in a list of key/value pairs and return the set of decrypted the results
      *
      * @param array $data
