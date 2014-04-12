b1597164fdd421f82427d2ab78ee34aa6399c928
Craig Giles <craig@gilesc.com>
2014-04-12 11:32:13 -0700

    added unit tests for registration validator

--- app/redhotmayo/registration/RegistrationValidator.php
+++ app/redhotmayo/registration/RegistrationValidator.php
@@ -4,6 +4,10 @@ use redhotmayo\validation\ValidationException;
 use redhotmayo\validation\Validator;
 
 class RegistrationValidator extends Validator {
+    const USERNAME = 'username';
+    const PASSWORD = 'password';
+    const EMAIL = 'email';
+
     public function getCreationRules() {
         return [
             'username' => "required|min:5|unique:users,username",
