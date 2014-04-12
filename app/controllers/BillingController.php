e820d17294188bc8f1c42da01b51fc321da7286d
Craig Giles <craig@gilesc.com>
2014-04-12 16:32:44 -0700

    WS-15 added stripe billing controller and manager

--- app/controllers/BillingController.php
+++ app/controllers/BillingController.php
@@ -1,20 +1,50 @@
 <?php
 
 use Illuminate\Support\Facades\Input;
-use redhotmayo\dataaccess\repository\BillingRepository;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redirect;
+use redhotmayo\billing\BillingManager;
+use redhotmayo\billing\exception\BillingException;
+use redhotmayo\model\Billing;
 
 class BillingController extends BaseController {
-    /** @var BillingRepository $billingRepo */
-    private $billingRepo;
+    const BILLING_TOKEN = 'stripeToken';
 
-    function __construct(BillingRepository $billingRepo) {
-        $this->billingRepo = $billingRepo;
+    private $billingManager;
+
+    function __construct(BillingManager $billingManager) {
+        $this->billingManager = $billingManager;
     }
 
     public function index() {
+        return View::make('billing.index');
     }
 
     public function store() {
-        dd(Input::all());
+        try {
+            $token = $this->getBillingToken();
+            $user = Auth::user();
+
+            $this->billingManager->addBasicSubscription($user, $token);
+        } catch (BillingException $billException) {
+            Log::error("BillingException: {$billException->getMessage()}");
+            $this->redirectWithErrors($billException->getErrors());
+        }
+    }
+
+    private function getBillingToken() {
+        $token = Input::get(self::BILLING_TOKEN);
+
+        if (!isset($token)) {
+            $this->redirectWithErrors('Billing Token Not Found');
+        }
+
+       return $token;
+    }
+
+    private function redirectWithErrors($message, $input=[]) {
+        Redirect::action('BillingController@index')
+                ->withInput($input)
+                ->withErrors($message);
     }
 }
