933943a051967bacdd6df21209b9989d35471733
scottlivingston <s.livingst@gmail.com>
2014-04-13 15:27:35 -0700

    WS-17 testing form submission without ajax

--- app/views/registration/index.blade.php
+++ app/views/registration/index.blade.php
@@ -15,49 +15,90 @@
 <div>{{ $error }}</div>
 @endforeach
 @endif
+<div class="row">
+  <div class="col-md-6">
+  <h3>New User</h3>
+  <hr>
+  {{ Form::open(['url' => 'registration', 'autocomplete' => 'off', 'role'=>'form', 'id'=>'new_user']) }}
 
-{{ Form::open(['url' => 'registration', 'autocomplete' => 'off', 'role'=>'form', 'id'=>'form']) }}
-
-  <div class="form-group row">
-    <div class="col-md-12 col-lg-6">
-      <label for="username" class="control-label">User Name:</label>
-      <input type="text" name="username" id="username" class="form-control">
+    <div class="form-group row">
+      <div class="col-lg-12">
+        <label for="username" class="control-label">User Name:</label>
+        <input type="text" name="username" id="username" class="form-control">
+      </div>
     </div>
-  </div>
 
-  <div class="form-group row">
-    <div class="col-md-12 col-lg-6">
-      <label for="password" class="control-label">Password:</label>
-      <input type="password" name="password" id="password" class="form-control">
+    <div class="form-group row">
+      <div class="col-lg-12">
+        <label for="password" class="control-label">Password:</label>
+        <input type="password" name="password" id="password" class="form-control">
+      </div>
     </div>
-  </div>
 
-  <div class="form-group row">
-    <div class="col-md-12 col-lg-6">
-      <label for="password_confirmation" class="control-label">Password Confirmation:</label>
-      <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" data-validation-match-match="password">
+    <div class="form-group row">
+      <div class="col-lg-12">
+        <label for="password_confirmation" class="control-label">Password Confirmation:</label>
+        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" data-validation-match-match="password">
+      </div>
     </div>
-  </div>
 
-  <div class="form-group row">
-    <div class="col-md-12 col-lg-6">
-      <label for="email" class="control-label">Email:</label>
-      <input type="email" name="email" id="email" class="form-control">
+    <div class="form-group row">
+      <div class="col-lg-12">
+        <label for="email" class="control-label">Email:</label>
+        <input type="email" name="email" id="email" class="form-control">
+      </div>
     </div>
-  </div>
 
-  <div class="form-group row">
-    <div class="col-md-12 col-lg-6">
-      <button type="submit" class="btn btn-primary btn-lg pull-right">Register</button>
+    <div class="form-group row">
+      <div class="col-lg-12">
+        <label for="key" class="control-label">Key:</label>
+        <input type="key" name="key" id="key" class="form-control">
+      </div>
     </div>
+
+    <div class="form-group row">
+      <div class="col-lg-12">
+        <button type="submit" class="btn btn-primary btn-lg pull-right">Register</button>
+      </div>
+    </div>
+
+  {{ Form::close() }}
   </div>
-{{ Form::close() }}
+  <div class="col-md-5 col-md-offset-1">
+    <h3>Returning User</h3>
+    <hr>
+    {{ Form::open(array('route' => 'sessions.store')) }}
+
+      <div class="form-group row">
+        <div class="col-lg-12">
+          <label for="username" class="control-label">User Name:</label>
+          <input type="text" name="username" id="username" class="form-control">
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <div class="col-lg-12">
+          <label for="password" class="control-label">Password:</label>
+          <input type="password" name="password" id="password" class="form-control">
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <div class="col-lg-12">
+          <button type="submit" class="btn btn-primary btn-lg pull-right">Login</button>
+        </div>
+      </div>
+<!--      {{ link_to_route('password_resets.create', 'Forgot your password') }}-->
+    {{ Form::close() }}
+  </div>
+</div>
+
 @endsection
 
 @section('javascript')
 <script>
   $(document).ready(function() {
-    $('#form').bootstrapValidator({
+    $('#new_user').bootstrapValidator({
       fields: {
         username: {
           validators: {
@@ -97,11 +138,34 @@
               message: 'The input is not a valid email address'
             }
           }
-        },
-      },
-      submitHandler: function(validator, form, submitButton) {
-
+        }
       }
+//      submitHandler: function(validator, form, submitButton) {
+//        var formObject = {
+//          username: $('[name=]'),
+//          password:'',
+//          password_confirmation:'',
+//          email:'',
+//          _token:'',
+//          key:''
+//        };
+//
+//        $.ajax({
+//          url: 'registration',
+//          type: 'POST',
+//          dataType: 'json',
+//          data: JSON.stringify(formObject),
+//          cache: true,
+//          complete: function(data) {
+//            if (data.status === 401 || data.status === 200) {
+//              if (data.responseJSON.redirect){
+//                window.location.href = data.responseJSON.redirect;
+//              }
+//            } else {
+//            }
+//          }
+//        });
+//      }
     });
   });
 </script>
