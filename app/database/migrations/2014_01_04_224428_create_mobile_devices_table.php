0b32e716354652a1a005223b331952acd11a85b5
Craig Giles <craig@gilesc.com>
2014-04-24 18:54:37 -0700

    Changed mobile device type to blob

--- app/database/migrations/2014_01_04_224428_create_mobile_devices_table.php
+++ app/database/migrations/2014_01_04_224428_create_mobile_devices_table.php
@@ -17,7 +17,7 @@ class CreateMobileDevicesTable extends Migration {
             $table->increments('id');
             $table->unsignedInteger('userId');
             $table->string('deviceType', 15);
-            $table->string('installationId', 500)->nullable()->unique();
+            $table->binary('installationId', 500)->nullable();
             $table->string('appVersion', 20);
 
             $table->foreign('userId')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
