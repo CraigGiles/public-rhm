4308b63feeb89aa19a6076efc8f67703c771a357
Craig Giles <craig@gilesc.com>
2014-04-27 11:16:25 -0700

    added authorization service
    Starting down the road of deprecating the ApiAuthorizor for this new Authorization Service. Added unit tests around it as well

---
+++ app/redhotmayo/auth/AuthorizationServiceProvider.php
@@ -0,0 +1,35 @@
+<?php  namespace redhotmayo\auth;
+
+use Illuminate\Support\Facades\App;
+use redhotmayo\provider\RedHotMayoServiceProvider;
+
+class AuthorizationServiceProvider extends RedHotMayoServiceProvider {
+    const SERVICE_NAME = 'authorization';
+    const ALIAS_NAME = 'Authorization';
+    const ALIAS_VALUE = 'redhotmayo\auth\AuthorizationService';
+
+    /**
+     * Sets up any dependencies needed to construct the service and returns
+     * the newly constructed service to the caller
+     *
+     * @return mixed
+     *
+     * @author Craig Giles < craig@gilesc.com >
+     */
+    public function getService() {
+        $userRepo = App::make('redhotmayo\dataaccess\repository\UserRepository');
+        return new AuthorizationService($userRepo);
+    }
+
+    public function getServiceName() {
+        return self::SERVICE_NAME;
+    }
+
+    public function getAliasName() {
+        return self::ALIAS_NAME;
+    }
+
+    public function getAliasValue() {
+        return self::ALIAS_VALUE;
+    }
+}
