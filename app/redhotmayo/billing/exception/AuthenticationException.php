db7eb22dc47c68ae665614dabcd8679a97e57366
Craig Giles <craig@gilesc.com>
2014-05-03 18:09:33 -0700

    WS-15 removed two exceptions

--- app/redhotmayo/billing/exception/AuthenticationException.php
+++
@@ -1,6 +0,0 @@
-<?php  namespace redhotmayo\billing\exception;
-
-use redhotmayo\exception\Exception;
-
-class AuthenticationException extends Exception {
-} 
--- app/redhotmayo/billing/exception/InvalidRequestException.php
+++
@@ -1,6 +0,0 @@
-<?php  namespace redhotmayo\billing\exception;
-
-use redhotmayo\exception\Exception;
-
-class InvalidRequestException extends Exception {
-} 
