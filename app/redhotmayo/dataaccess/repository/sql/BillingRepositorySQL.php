2b8ac172173e0fc6c2ee6c38f5cafe051053e05c
Craig Giles <craig@gilesc.com>
2014-05-11 18:30:08 -0700

    WS-15 re-implmeented the getCustomerToken method

--- app/redhotmayo/dataaccess/repository/sql/BillingRepositorySQL.php
+++ app/redhotmayo/dataaccess/repository/sql/BillingRepositorySQL.php
@@ -49,13 +49,13 @@ class BillingRepositorySQL extends RepositorySQL implements BillingRepository {
     }
 
     public function getCustomerToken(User $user) {
-//        $token = (array)DB::table($this->getTableName())
-//            ->select(BillingStripeSQL::C_CUSTOMER_TOKEN)
-//            ->where(BillingStripeSQL::C_USER_ID, '=', $user->getId())
-//            ->first();
-//
-//        $decryptedRow = $this->dao->decrypt($token);
-//        return Arrays::GetValue($decryptedRow, BillingStripeSQL::C_CUSTOMER_TOKEN, $this->getUnknownCustomerToken());
+        $token = (array)DB::table($this->getTableName())
+            ->select(BillingStripeSQL::C_CUSTOMER_TOKEN)
+            ->where(BillingStripeSQL::C_ID, '=', $user->getBillingId())
+            ->first();
+
+        $decryptedRow = $this->dao->decrypt($token);
+        return Arrays::GetValue($decryptedRow, BillingStripeSQL::C_CUSTOMER_TOKEN, $this->getUnknownCustomerToken());
     }
 
     /**
