b67e01ca3b828b1424772d248ca4aeeea809b177
Craig Giles <craig@gilesc.com>
2014-08-11 21:01:07 -0700

    changed login page to be a blade page

--- app/views/layouts/login.blade.php
+++ app/views/layouts/login.blade.php
@@ -2,22 +2,22 @@
 
 <div class="form-group row">
   <div class="col-lg-12">
-    <label for="username" class="control-label">User Name:</label>
-    <input type="text" name="username" id="username" class="form-control">
+    {{ Form::input("text", "username", null, ["class" => "form-control", "placeholder" => "User Name"]) }}
   </div>
 </div>
 
 <div class="form-group row">
   <div class="col-lg-12">
-    <label for="password" class="control-label">Password:</label>
-    <input type="password" name="password" id="password" class="form-control">
+    {{ Form::input("password", "password", null, ["class" => "form-control", "placeholder" => "Password"]) }}
   </div>
 </div>
 
 <div class="form-group row">
   <div class="col-lg-12">
-    <button id="signin_submit" type="submit" class="btn btn-primary btn-lg pull-right">Login</button>
+    {{ Form::button("Submit", ["type" => "submit", "class" => "btn btn-primary btn-lg"]) }}
+    {{ link_to_route('password_resets.create', 'Forgot your password') }}
   </div>
 </div>
-<!--      {{ link_to_route('password_resets.create', 'Forgot your password') }}-->
+
+
 {{ Form::close() }}
--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -3,73 +3,30 @@
 @section('content')
 
 <div class="page-header">
+@if (Session::has('flash_message'))
+    <div class="alert alert-info">
+        {{ Session::get('flash_message') }}
+        @include('partials.errors')
+    </div>
+@endif
   <h1>
-    Sign In
+    Log In
   </h1>
 </div>
 
-<div class="row">
 
-  <div class="col-lg-7">
+  <div class="col-lg-5">
     @include('layouts.login')
   </div>
 
-  <div class="col-md-5">
-    <img src="http://redhotmayo.com/wp-content/uploads/2014/03/Phone-Tablet-Launch-Page-image-portrait-v2.png" width="100%"/>
+  <div class="col-md-7">
+    redhotMAYO's new "HOT" module gives you the ability to integrate with suppliers and brokers in ways never before possible, including:
+    <p>
+        <li>Share key account details with partners to collaborate in tag team selling</li>
+        <li>Automate your sampling processes to get samples to operators and close new sales faster</li>
+        <li>Other marketing messages....</li>
+    </p>
   </div>
 
-</div>
-
-@endsection
-
-
-@section('javascript')
-
-<script>
-  $(document).ready(function() {
-    /**
-     * Signin Form validation
-     */
-    $('#signin_form').bootstrapValidator({
-      fields: {
-        username: {
-          validators: {
-            notEmpty: {
-              message: 'This field is required and cannot be empty'
-            }
-          }
-        },
-        password: {
-          validators: {
-            notEmpty: {
-              message: 'This field is required and cannot be empty'
-            }
-          }
-        }
-      },
-      submitHandler: function(validator, form, submitButton) {
-
-        $.ajax({
-          url: $('#signin_form').attr('action'),
-          type: $('#signin_form').attr('method'),
-          data: form.serialize(),
-          beforeSend: function() {
-            beforeAjax();
-          },
-          complete: function(data) {
-            $('#signin_submit').prop('disabled', false);
-            if (data.status === 200) {
-              if (data.responseJSON.redirect){
-                window.location.href = data.responseJSON.redirect;
-              }
-            } else {
-              handleResponse(data);
-            }
-          }
-        });
-      }
-    });
-  });
-</script>
 
 @endsection
