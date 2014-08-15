1dce5649de5cc44b071eed2fa835c632ccb216ee
Craig Giles <craig@gilesc.com>
2014-08-14 20:21:56 -0700

    updated route to subscribe instead of subscription update

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -5,7 +5,7 @@
 
     <h2>Dashboard</h2>
 
-    {{ link_to('subscription/update', 'Update Regions', ['class' => 'btn btn-primary btn-lg']) }}
+    {{ link_to('subscribe', 'Update Regions', ['class' => 'btn btn-primary btn-lg']) }}
     {{ link_to('billing/cancel', 'Cancel Subscription', ['class' => 'btn btn-primary btn-lg']) }}
 @endsection
 @stop
