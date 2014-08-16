db24a518fc27359e3da43b7df42052b70022940c
Craig Giles <craig@gilesc.com>
2014-08-15 20:51:37 -0700

    formatting

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,29 +1,30 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no">
-    <title>Red Hot Mayo</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no">
+        <title>Red Hot Mayo</title>
 
-    {{ stylesheet_link_tag() }}
-    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-    @yield('css')
-  </head>
-<body>
-
-    @include('partials.unauth-banner')
-
-
-  <div class="container">
-    <div id="warnings"></div>
-    @yield('content')
-  </div>
-  <div class="footer">
-
-  </div>
+        {{ stylesheet_link_tag() }}
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+        @yield('css')
+    </head>
+    <body>
+        @include('partials.unauth-banner')
 
 
-    {{ javascript_include_tag() }}
-    @yield('javascript')
-</body>
+        <div class="container">
+            <div id="warnings">
+            </div>
+
+            @yield('content')
+        </div>
+
+        <div class="footer">
+        </div>
+
+
+        {{ javascript_include_tag() }}
+        @yield('javascript')
+    </body>
 </html>
