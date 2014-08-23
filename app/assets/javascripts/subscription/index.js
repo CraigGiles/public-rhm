4cb935e97a00a6738778166f623a175b712d0355
Craig Giles <craig@gilesc.com>
2014-08-23 08:48:09 -0700

    refactored javascript to be more DRY

--- app/assets/javascripts/subscription/index.js
+++ app/assets/javascripts/subscription/index.js
@@ -3,17 +3,28 @@ RHM.App = RHM.App || {}
 
 RHM.App.Subscription = {
     statesFilter: function() {
-        $('#dropdown-states').keyup(function(){
-            var valThis = $(this).val();
+        $('#dropdown-states').keyup(function() {
+            RHM.App.Subscription.filter($(this));
+        });
+    },
 
-            $( "li" ).each(function() {
-                var text = $(this).text().toLowerCase();
-                (text.indexOf(valThis) > -1) ? $(this).show() : $(this).hide();
-            });
+    countyFilter: function() {
+        $('#dropdown-counties').keyup(function() {
+            RHM.App.Subscription.filter($(this));
+        });
+    },
+
+    filter: function(object) {
+        var valThis = object.val();
+
+        $( "li" ).each(function() {
+            var text = $(this).text().toLowerCase();
+            (text.indexOf(valThis) > -1) ? $(this).show() : $(this).hide();
         });
     }
 }
 
 $(document).ready(function (){
     RHM.App.Subscription.statesFilter();
+    RHM.App.Subscription.countyFilter();
 });
