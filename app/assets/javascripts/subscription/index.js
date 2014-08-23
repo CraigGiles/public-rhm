b65dbaa011d8471326b0af64a3b2d1deadbc6c6c
Craig Giles <craig@gilesc.com>
2014-08-23 07:55:22 -0700

    updated javascript to use better namespaces

--- app/assets/javascripts/subscription/index.js
+++ app/assets/javascripts/subscription/index.js
@@ -1,17 +1,19 @@
 var RHM = RHM || {}
 RHM.App = RHM.App || {}
-RHM.App.Subscription = RHM.App.Subscription || {}
+
+RHM.App.Subscription = {
+    statesFilter: function() {
+        $('#dropdown-states').keyup(function(){
+            var valThis = $(this).val();
+
+            $( "li" ).each(function() {
+                var text = $(this).text().toLowerCase();
+                (text.indexOf(valThis) > -1) ? $(this).show() : $(this).hide();
+            });
+        });
+    }
+}
 
 $(document).ready(function (){
+    RHM.App.Subscription.statesFilter();
 });
-
-$('#dropdown-states').keyup(function(){
-    var valThis = $(this).val();
-
-    $( "li" ).each(function() {
-        var text = $(this).text().toLowerCase();
-        (text.indexOf(valThis) > -1) ? $(this).show() : $(this).hide();
-    });
-});
-
-
