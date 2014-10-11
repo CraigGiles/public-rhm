4e930554e8a26d7aea4723cd002334cdf01681ae
Craig Giles <craig@gilesc.com>
2014-10-11 15:44:42 -0700

    Added tablet picture on dashboard page

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -6,7 +6,13 @@
 
     {{ link_to('subscribe', 'Update Regions', ['class' => 'btn btn-primary btn-lg']) }}
     {{ link_to('billing/cancel', 'Cancel Subscription', ['class' => 'btn btn-primary btn-lg']) }}
+
+<div class="col-md-4 pull-right app-image" style="margin-top: -80px">
+    <img src="http://redhotmayo.com/wp-content/uploads/2014/03/Phone-Tablet-Launch-Page-image-portrait-v2.png" width="100%"/>
+</div>
+
 @endsection
+
 @stop
 
 @section('javascript')
