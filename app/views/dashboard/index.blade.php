fae718acf5147d0aacec78b8b187f1e4d579ec36
jweyer <jweyer@snap-software.com>
2014-10-18 21:02:32 -0700

    Added the Upload page and Logout button.  Modifications pending review.

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -6,6 +6,15 @@
 
     {{ link_to('subscribe', 'Update Regions', ['class' => 'btn btn-primary btn-lg']) }}
     {{ link_to('billing/cancel', 'Cancel Subscription', ['class' => 'btn btn-primary btn-lg']) }}
+
+    <?php
+		$username = Auth::user()->username;
+		$usernames = array('amye', 'donv', 'dgiles', 'cgiles', 'jweyer');
+	?>
+    @if (in_array($username, $usernames))
+		{{ link_to('administration', 'Upload Leads', ['class' => 'btn btn-primary btn-lg']) }}
+	@endif
+
 @endsection
 @stop
 
--- app/views/partials/unauth-banner.blade.php
+++ app/views/partials/unauth-banner.blade.php
@@ -1,10 +1,15 @@
   <div class="banner">
     <div class="container banner-image">
       <div class="row">
-        <div class="col-sm-12">
+        <div class="col-sm-3">
           <div class="logo">
           </div>
         </div>
+        @if (Auth::check())
+       		<div class="logout-button">
+       			{{ link_to('logout', 'Logout', ['class' => 'btn btn-primary btn-sm']) }}
+       		</div>
+		@endif
       </div>
       <div class="row">
       </div>
