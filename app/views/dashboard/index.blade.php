5fab20f6650bb89859802f20161781ec57ee865d
Daniel Giles <dgiles05@gmail.com>
2014-10-21 20:00:42 -0700

    Added Playstore link to dashboard

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -20,10 +20,20 @@
 	    <font style="color: #ffffff; font-family:robotoregular;">
 	        {{ link_to('billing/cancel', 'Cancel my Subscription') }}
 	    </font>
-	
+
+        <div class="break"></div>
+        <p style = "display:block; padding-top:40px">
+            Thank you for using redhotMAYO!
+        </p>
+        <p>
+            To get access to all your leads, please download our app at the  <a href="https://play.google.com/store/apps/details?id=com.redhotmayo.accountManager&hl=en">Google Play Store</a>
+	    </p>
+        <p>
+            You can log into the app with the same username and password you used to sign up
+        </p>
 	</div>
 
-	<div class="col-md-4 pull-right app-image" style="margin-top: -140px">
+	<div class="col-md-4 pull-right app-image" style="margin-top: -270px; float: right;">
 	    <img src="assets/tablets.png"/>
 	</div>
 @endsection
